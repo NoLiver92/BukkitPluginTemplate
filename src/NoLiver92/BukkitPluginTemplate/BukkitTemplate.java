6d59f141ea19f54fdcea01577d7c4ec7dfe8fc84
NoLiver92 <nick.picturebox@talktalk.net>
2014-01-03 20:11:53 +0000

    Created basic main structure

--- src/NoLiver92/BukkitPluginTemplate/BukkitTemplate.java
+++ src/NoLiver92/BukkitPluginTemplate/BukkitTemplate.java
@@ -1,7 +1,19 @@
 package NoLiver92.BukkitPluginTemplate;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by Nick on 03/01/14.
  */
-public class BukkitTemplate {
+public class BukkitTemplate extends JavaPlugin
+{
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
 }
